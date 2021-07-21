6e26feab937d0dcb681c6d928a9768342bbeeb6d
Doug Pulse <PulseD@WSDOT.WA.GOV>
2021-07-21 14:32:26 -0700

    Improved MSAD lookups with recursion.

--- MSADGroupsPerUser.ps1
+++ MSADGroupsPerUser.ps1
@@ -1,11 +1,64 @@
-﻿$userName = Read-Host "user name"
+﻿function Get-GroupsForObject {
+    [cmdletbinding()]
+    param(
+        [string]$Object = "", 
+        [int]$Level = 0
+    )
+    
+    $d = Get-ADObject -Identity $Object -Properties SamAccountName
+ 
+    if ($d.ObjectClass -eq "user" -and $Level -eq 0) {
+        $e = Get-ADUser -Identity $d.DistinguishedName -Properties MemberOf
+    }
+		elseif ($d.ObjectClass -eq "group") {
+        $e = Get-ADGroup -Identity $d.DistinguishedName -Properties MemberOf
+    }
+ 
+    $e.MemberOf | Sort-Object | %{
+        # prevent a loop if the group is a member of itself
+        if ( $_ -ne $e.DistinguishedName ) {
+            Get-GroupsForObject -Object $_  -Level($Level + 1)
+        }
+    }
+    $e | select name | Sort-Object -Property name
+}
 
-$groups = Get-ADPrincipalGroupMembership "$userName" | select name | Sort-Object -Property name
+function RemoveDups {
+    [cmdletbinding()]
+    param (
+        [parameter(Mandatory=$true)]
+        [System.Collections.ArrayList]$ArrayList
+    )
+    $last = ""
+    $this = ""
+    [System.Collections.ArrayList]$out = @()
+    foreach ($a in $ArrayList) {
+        if ($a -ne $last) {
+            $null = $out.add($a)
+        }
+        $last = $a
+    }
+    $out
+}
 
+$userName = Read-Host "user name"
 $un = $userName -replace " ", ""
+$fn = (New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path + "\" + $un + "Groups.csv"
+$g = Get-GroupsForObject -Object (Get-ADuser $userName).DistinguishedName | Sort-Object -Property name
 
-$fn = $env:USERPROFILE + "\" + $un + "Groups.csv"
+[System.Collections.ArrayList]$gp = @()
+foreach ($m in $g) {
+    [void]$gp.Add($m.name)
+}
 
-$groups | Export-Csv -Path $fn -NoTypeInformation
+$groups = RemoveDups -ArrayList $gp
+
+#$groups | Export-Csv -Path $fn -NoTypeInformation
+
+Set-Content $fn -Value $null
+
+foreach ($e in $groups) {
+    "`"$e`"" | Add-Content $fn
+}
 
 Start-Process -FilePath $fn
--- MSADUsersPerGroup.ps1
+++ MSADUsersPerGroup.ps1
@@ -1,13 +1,67 @@
-﻿$groupName = Read-Host "group name"
+﻿function Get-UsersInGroup {
+    [cmdletbinding()]
+    param (
+        [parameter(Mandatory=$true)]
+        [string]$Object
+    )
+    
+    [System.Collections.ArrayList]$Users = @()
+ 
+    $x = Get-ADObject -Identity $Object -Properties SamAccountName
+ 
+    if ($x.ObjectClass -eq "group") {
+        $y = Get-ADGroup -Identity $Object -Properties Members
+ 
+        $y.Members | %{
+            $o = Get-ADObject -Identity $_ -Properties SamAccountName
+ 
+            if ($o.ObjectClass -eq "user") {
+                $null = $Users.Add($o)
+            }
+            elseif ($o.ObjectClass -eq "group") {
+                Get-UsersInGroup -Object $o.DistinguishedName
+            }
+        }
+    } else {
+        Write-Warning "$($Object) is not a group, it is a $($x.ObjectClass)"
+    }
+    $Users | select name | Sort-Object -Property name
+}
 
-$groupMembers = Get-ADGroupMember -Identity "$groupName" | Select-Object objectClass, name, SamAccountName
+function RemoveDups {
+    [cmdletbinding()]
+    param (
+        [parameter(Mandatory=$true)]
+        [System.Collections.ArrayList]$ArrayList
+    )
+    $last = ""
+    $this = ""
+    [System.Collections.ArrayList]$out = @()
+    foreach ($a in $ArrayList) {
+        if ($a -ne $last) {
+            $null = $out.add($a)
+        }
+        $last = $a
+    }
+    $out
+}
 
-#$groupCSV = ($groupMembers | Select-Object objectClass, name, SamAccountName)
-
+$groupName = Read-Host "group name"
 $gn = $groupName -replace " ", ""
+$fn = (New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path + "\" + $gn + "Members.csv"
+$gm = Get-UsersInGroup -Object (Get-ADGroup "$groupName").DistinguishedName | Sort-Object -Property name
 
-$fn = $env:USERPROFILE + "\" + $gn + "Members.csv"
+[System.Collections.ArrayList]$gpMem = @()
+foreach ($m in $gm) {
+    [void]$gpMem.Add($m.name)
+}
 
-$groupMembers | Export-Csv -Path $fn -NoTypeInformation
+$gpMembers = RemoveDups -ArrayList $gpMem
+
+Set-Content $fn -Value $null
+
+foreach ($e in $gpMembers) {
+    "`"$e`"" | Add-Content $fn
+}
 
 Start-Process -FilePath $fn
